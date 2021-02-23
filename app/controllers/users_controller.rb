f246f235b94ccd2c7fd0b8fcc74be8d5a1bccde9
Pauloparakleto <alparakleto@gmail.com>
2021-02-23 19:06:23 -0300

    Only admin can get user index else redirect user to root

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-  #def index
-   # @users = User.all
-  #end
+  def index
+    if current_user.admin?
+      @users = User.all
+    else
+      redirect_to root_path
+    end
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   config.eager_load = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  config.action_mailer.raise_delivery_errors = false
 
   # Show full error reports.
   config.consider_all_requests_local = true
