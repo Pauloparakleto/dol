b47a170f96d90832807963d98ff56217ee1a49e3
Pauloparakleto <alparakleto@gmail.com>
2021-02-24 13:29:15 -0300

    Change routes order: first comes devise for users then get users and get show users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get '/users', to: "users#index"
-  get "/users/:id", to: "users#show"
   devise_for :users
   root to: 'home#welcome'
+  get '/users', to: "users#index"
+  get "/users/:id", to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
