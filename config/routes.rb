ef2614759c5632c79993e05a7f3023af8275bacf
Pauloparakleto <alparakleto@gmail.com>
2021-02-24 13:09:07 -0300

    Add get show to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  get '/users', to: "users#index"
+  get "/users/:id", to: "users#show"
   devise_for :users
   root to: 'home#welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
