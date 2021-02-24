e65b1875c069fda860d11fb53152ed8c9bbbd75f
Pauloparakleto <alparakleto@gmail.com>
2021-02-24 13:08:33 -0300

    Add show and banished action. This one need test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,24 @@ class UsersController < ApplicationController
       redirect_to root_path
     end
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def banish_user
+    @article = Article.find(params[:id])
+    @article.banished = 1
+    if @article.save
+      redirect_to index_user_path
+    else
+      render index_user_path, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:banished)
+  end
 end
