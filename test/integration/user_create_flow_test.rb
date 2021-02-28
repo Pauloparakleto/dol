af73c340142cc7a1258f9b1c1a92a7020955b22f
Paulo Felipe <alparakleto@gmail.com>
2021-02-28 18:09:18 -0300

    Add test to user registration flow after permit name params to devise

--- test/integration/user_create_flow_test.rb
+++ test/integration/user_create_flow_test.rb
@@ -1,12 +1,55 @@
 require "test_helper"
 
 class UserCreateFlowTest < ActionDispatch::IntegrationTest
-   test "Should create valid user" do
+ 
+  test "Should not create user with empity email" do
     get new_user_registration_path
      assert_response :success
 
      post "/users",
-     params: { user: { name: "Louis", email: "louis.camoes@gmail.com", 
+     params: { user: { name: "ab", email: "", 
+                                      password: "ronaldinho3", 
+                                      password_confirmation: "ronaldinho3"} }
+      assert_response :success      
+   end
+
+  test "Should not create user with name equal to empy string" do
+    get new_user_registration_path
+     assert_response :success
+
+     post "/users",
+     params: { user: { name: "", email: "louis.camoes@gmail.com", 
+                                      password: "ronaldinho3", 
+                                      password_confirmation: "ronaldinho3"} }
+      assert_response :success      
+   end
+
+  test "Should not create user without name to short" do
+    get new_user_registration_path
+     assert_response :success
+
+     post "/users",
+     params: { user: { name: "a", email: "louis.camoes@gmail.com", 
+                                      password: "ronaldinho3", 
+                                      password_confirmation: "ronaldinho3"} }
+      assert_response :success      
+   end
+  test "Should not create user without name" do
+    get new_user_registration_path
+     assert_response :success
+
+     post "/users",
+     params: { user: { name: nil, email: "louis.camoes@gmail.com", 
+                                      password: "ronaldinho3", 
+                                      password_confirmation: "ronaldinho3"} }
+      assert_response :success      
+   end 
+  test "Should create valid user" do
+    get new_user_registration_path
+     assert_response :success
+
+     post "/users",
+     params: { user: { name: "Louis2", email: "louis.camoes3@gmail.com", 
                                       password: "ronaldinho3", 
                                       password_confirmation: "ronaldinho3"} }
       assert_response :redirect
