8feab68115552db9044dfd7b48f8cc738c069d07
Paulo Felipe <alparakleto@gmail.com>
2021-02-28 18:08:10 -0300

    Add validates to user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, presence: true, length: { minimum: 2, maximum: 15 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
