540c1fd1fef5ff67344b1f9debc436e62b92646e
Paulo Felipe <alparakleto@gmail.com>
2021-03-11 19:26:44 -0300

    Add user_banished? method to Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :user_banished?, if: :devise_controller?
 
     protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def user_banished?
+    if user_signed_in?
+      if current_user.banished?
+        redirect_to banished_path
+        sign_out current_user
+      end
+    end
   end
 
 end
