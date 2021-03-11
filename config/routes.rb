d187c87a57883eb7ebce55edcb5a5c791ba7cee3
Paulo Felipe <alparakleto@gmail.com>
2021-03-11 18:30:27 -0300

    Add rout to banish controller with verb post

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: 'home#welcome'
   get '/users', to: "users#index"
   get "/users/:id", to: "users#show"
+  post "banish/:id", to: "banish#banish", as: "banish"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
