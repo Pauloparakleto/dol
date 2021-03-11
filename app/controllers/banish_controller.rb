083f5f0a136250e4b2603035d9ff37ad011818b6
Paulo Felipe <alparakleto@gmail.com>
2021-03-11 18:31:08 -0300

    Add controller Banish and banish action. Need sanitization on params maybe

---
+++ app/controllers/banish_controller.rb
@@ -0,0 +1,20 @@
+class BanishController < ApplicationController
+    def banish
+        @user = User.find(params[:id])
+        @user.banished = true
+       if current_user.admin? && @user.save
+        puts "User banished"
+        redirect_to users_path
+       else
+        puts "Unprocessed entity"
+        redirect_to users_path
+       end
+    end
+
+    private
+
+  def user_params
+    params.require(:user).permit(:banished)
+  end
+
+end
