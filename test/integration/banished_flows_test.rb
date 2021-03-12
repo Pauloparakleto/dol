8c49eaef975b0b6fa3a10a50daae7ceae3787043
Paulo Felipe <alparakleto@gmail.com>
2021-03-12 20:00:44 -0300

    Add test banished user can not get root and it is redirect to banished way

--- test/integration/banished_flows_test.rb
+++ test/integration/banished_flows_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class BanishedFlowsTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+   test "Not banished user can get root_path" do
+    sign_in users(:two)
+    get root_path
+    assert_response :success
+   end
+
+   test "Banished user should redirect to banished way" do
+    sign_in users(:four)
+    get root_path
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+   end
 end
