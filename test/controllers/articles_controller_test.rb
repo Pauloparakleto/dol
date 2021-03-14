3de0866feeda335af65b3acd5c2e278892203ae0
Paulo Felipe <alparakleto@gmail.com>
2021-03-14 17:27:40 -0300

    test for should ge index articles with a valid current_user

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   test "should get index" do
+    sign_in users(:one)
     get articles_url
     assert_response :success
   end
