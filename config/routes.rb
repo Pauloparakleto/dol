0789e6498e474aa0037ec35d53734e98d406e358
Paulo Felipe <alparakleto@gmail.com>
2021-03-14 17:18:08 -0300

    resource for articles

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
   get "/users/:id", to: "users#show"
   get "/banished/", to: "banish#banished_way"
   post "/banish/:id", to: "banish#banish", as: "banish"
+
+  #Routes for notícias
+  resources :articles
+ # get 'articles/index'
+  #get "articles/index/:id", to: "articles#show"
+  #get "articles/new"
+  #post "articles/new"
+  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
